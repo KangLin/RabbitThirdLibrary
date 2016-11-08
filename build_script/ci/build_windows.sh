28d3f0ad584af3ad7b83a6cc4c618d34ba92a792
KangLin <kl222@126.com>
2016-11-08 22:56:36 +0800

    add MSYSTEM in mingw

--- build_script/ci/build_windows.sh
+++ build_script/ci/build_windows.sh
@@ -3,14 +3,13 @@ set -ev
 
 if [  "$BUILD_TARGERT" = "windows_mingw" ]; then
     export PATH=/C/Qt/Tools/mingw${toolchain_version}_32/bin:$PATH
-    export LIBRARY_PATH=/C/Qt/Tools/mingw${toolchain_version}_32/i686-w64-mingw32/lib:$LIBRARY_PATH
-    export LDLIBRARY_PATH=/C/Qt/Tools/mingw${toolchain_version}_32/i686-w64-mingw32/lib:$LIBRARY_PATH
 fi
 
 SOURCE_DIR=`pwd`
 if [ -n "$1" ]; then
     SOURCE_DIR=$1
 fi
+
 SCRIPT_DIR=${SOURCE_DIR}/build_script
 if [ -d ${SOURCE_DIR}/ThirdLibrary/build_script ]; then
     SCRIPT_DIR=${SOURCE_DIR}/ThirdLibrary/build_script
@@ -54,15 +53,3 @@ do
         ./build_$v.sh ${BUILD_TARGERT} ${SOURCE_DIR}/$v #> /dev/null
     fi
 done
-#./build_webrtc.sh ${BUILD_TARGERT}
-
-#./build_openssl.sh ${BUILD_TARGERT} ${SOURCE_DIR}/openssl > /dev/null
-#./build_libcurl.sh ${BUILD_TARGERT} ${SOURCE_DIR}/curl #> /dev/null
-#./build_libvpx.sh ${BUILD_TARGERT} ${SOURCE_DIR}/libvpx > /dev/null
-#./build_libyuv.sh ${BUILD_TARGERT} ${SOURCE_DIR}/libyuv > /dev/null
-#./build_x264.sh ${BUILD_TARGERT} ${SOURCE_DIR}/libx264 > /dev/null
-#./build_ffmpeg.sh ${BUILD_TARGERT} ${SOURCE_DIR}/ffmpeg # > /dev/null
-#
-#./build_qxmpp.sh ${BUILD_TARGERT} ${SOURCE_DIR}/qxmpp > /dev/null
-#./build_qzxing.sh ${BUILD_TARGERT} ${SOURCE_DIR}/qzxing > /dev/null
-#./build_rabbitim.sh ${BUILD_TARGERT} ${SOURCE_DIR} qmake
