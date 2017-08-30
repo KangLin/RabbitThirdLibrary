a0dd05e7ed471f5815c06b0ba3e1efef24581203
KangLin <kl222@126.com>
2017-08-30 16:29:10 +0800

    Modify build_rabbitim.sh

--- build_script/build_rabbitim.sh
+++ build_script/build_rabbitim.sh
@@ -139,7 +139,7 @@ else #qmake编译
             fi
             
             if [ "${RABBIT_BUILD_HOST}"="windows" ]; then
-                MAKE="mingw32-make" # ${RABBIT_MAKE_JOB_PARA}"
+                MAKE="make" # ${RABBIT_MAKE_JOB_PARA}"
             fi
             
             ;;
