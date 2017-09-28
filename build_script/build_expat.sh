3f3af212f28870eb65c0f83a2590fd35d27806c9
KangLin <kl222@126.com>
2017-09-28 15:57:18 +0800

    modify expat version

--- build_script/build_expat.sh
+++ build_script/build_expat.sh
@@ -40,7 +40,7 @@ CUR_DIR=`pwd`
 
 #下载源码:
 if [ ! -d ${RABBIT_BUILD_SOURCE_CODE} ]; then
-    VERSION=R_2_2_4
+    VERSION=R_2_2_0
     if [ "TRUE" = "${RABBIT_USE_REPOSITORIES}" ]; then
         echo "git clone -q --branch=${VERSION} https://github.com/libexpat/libexpat.git ${RABBIT_BUILD_SOURCE_CODE}"
         git clone --branch=${VERSION} https://github.com/libexpat/libexpat.git ${RABBIT_BUILD_SOURCE_CODE}
