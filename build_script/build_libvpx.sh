a396af4ba08f0294fd5e06194284000631dcfa42
KangLin <kl222@126.com>
2018-05-28 10:54:38 +0800

    Modify build_libvpx.sh

--- build_script/build_libvpx.sh
+++ build_script/build_libvpx.sh
@@ -35,7 +35,7 @@ fi
 
 #下载源码:
 if [ ! -d ${RABBIT_BUILD_SOURCE_CODE} ]; then
-    VERSION=v1.7.0 #8446af7e9a12c5725e845564f40272dd9185c1cc
+    VERSION=v1.7.0 #ed217fe372f59eaeef074b4d3859b49a700cfa47
     echo "git clone -q --branch=$VERSION https://chromium.googlesource.com/webm/libvpx ${RABBIT_BUILD_SOURCE_CODE}"
     #git clone -q --branch=$VERSION https://chromium.googlesource.com/webm/libvpx ${RABBIT_BUILD_SOURCE_CODE}
     git clone -q https://chromium.googlesource.com/webm/libvpx ${RABBIT_BUILD_SOURCE_CODE}
@@ -72,7 +72,7 @@ case ${RABBIT_BUILD_TARGERT} in
         export CXX=${RABBIT_BUILD_CROSS_PREFIX}g++
         export AR=${RABBIT_BUILD_CROSS_PREFIX}gcc-ar
         export LD=${RABBIT_BUILD_CROSS_PREFIX}ld
-        export AS=${RABBIT_BUILD_CROSS_PREFIX}as
+        #export AS=${RABBIT_BUILD_CROSS_PREFIX}as
         export STRIP=${RABBIT_BUILD_CROSS_PREFIX}strip
         export NM=${RABBIT_BUILD_CROSS_PREFIX}nm
         export CROSS=${RABBIT_BUILD_CROSS_PREFIX}
@@ -82,14 +82,13 @@ case ${RABBIT_BUILD_TARGERT} in
                 CONFIG_PARA="${CONFIG_PARA} --target=armv7-android-gcc"
                 ;;
             x86*|arm64*)
-                CONFIG_PARA="${CONFIG_PARA} --as=auto --target=${RABBIT_ARCH}-android-gcc"
+                CONFIG_PARA="${CONFIG_PARA} --target=${RABBIT_ARCH}-android-gcc"
                 ;;
             *)
                 echo "Don't support target ${RABBIT_ARCH}"
                 exit 0
             ;;
         esac
-
         export CFLAGS="${RABBIT_CFLAGS}"
         export CPPFLAGS="${RABBIT_CPPFLAGS}"
         export LDFLAGS="${RABBIT_LDFLAGS}"
@@ -142,9 +141,13 @@ case ${RABBIT_BUILD_TARGERT} in
         exit 2
         ;;
 esac
-
+if [ "$RABBIT_BUILD_STATIC" = "static" ]; then
+    CONFIG_PARA="${CONFIG_PARA} --enable-static --disable-shared"
+else
+    CONFIG_PARA="${CONFIG_PARA} --disable-static --enable-shared"
+fi
 CONFIG_PARA="${CONFIG_PARA} --enable-libs --prefix=$RABBIT_BUILD_PREFIX"
-CONFIG_PARA="${CONFIG_PARA} --disable-docs --disable-examples --disable-install-docs"
+CONFIG_PARA="${CONFIG_PARA} --disable-docs --disable-examples --disable-install-docs --disable-tools "
 CONFIG_PARA="${CONFIG_PARA} --disable-install-bins --enable-install-libs"
 CONFIG_PARA="${CONFIG_PARA} --disable-unit-tests --disable-debug --disable-debug-libs"
 echo "../configure ${CONFIG_PARA} --extra-cflags=\"${CFLAGS=}\""
